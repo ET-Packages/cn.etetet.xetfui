63e49c5b04d26b17d89ba266b859448eecba646f
IcePower <icepower@git.com>
2025-01-22 15:17:26 +0800

    FUIComponent 增加了一些 Close 的接口

--- Scripts/HotfixView/Client/FairyGUI/FUICompoennt/FUIComponentSystem.Close.cs
+++ Scripts/HotfixView/Client/FairyGUI/FUICompoennt/FUIComponentSystem.Close.cs
@@ -28,31 +28,91 @@ namespace ET.Client
             }
         }
         
+        public static void ClosePanel<T>(this FUIComponent self) where T: Entity
+        {
+            PanelId panelId = self.GetPanelIdByGeneric<T>();
+            self.ClosePanel(panelId);
+        }
+        
+        public static void ClosePanel<T, P1>(this FUIComponent self, P1 p1) where T: Entity
+        {
+            PanelId panelId = self.GetPanelIdByGeneric<T>();
+            self.ClosePanel(panelId, p1);
+        }
+        
+        public static void ClosePanel<T, P1, P2>(this FUIComponent self, P1 p1, P2 p2) where T: Entity
+        {
+            PanelId panelId = self.GetPanelIdByGeneric<T>();
+            self.ClosePanel(panelId, p1, p2);
+        }
+        
+        public static void ClosePanel<T, P1, P2, P3>(this FUIComponent self, P1 p1, P2 p2, P3 p3) where T: Entity
+        {
+            PanelId panelId = self.GetPanelIdByGeneric<T>();
+            self.ClosePanel(panelId, p1, p2, p3);
+        }
+        
+        public static void ClosePanel<T, P1, P2, P3, P4>(this FUIComponent self, P1 p1, P2 p2, P3 p3, P4 p4) where T: Entity
+        {
+            PanelId panelId = self.GetPanelIdByGeneric<T>();
+            self.ClosePanel(panelId, p1, p2, p3, p4);
+        }
+        
+        public static void ClosePanel<T, P1, P2, P3, P4, P5>(this FUIComponent self, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5) where T: Entity
+        {
+            PanelId panelId = self.GetPanelIdByGeneric<T>();
+            self.ClosePanel(panelId, p1, p2, p3, p4, p5);
+        }
+        
         public static void ClosePanel(this FUIComponent self, long entityId)
         {
             FUIEntity fuiEntity = self.GetFUIEntity(entityId);
-            self.ClosePanel(fuiEntity);
+            self.ClosePanel(fuiEntity.PanelId);
         }
-
+        
         public static void ClosePanel(this FUIComponent self, PanelId panelId)
         {
             FUIEntity fuiEntity = self.GetFirstFUIEntityByPanelId(panelId);
-            self.ClosePanel(fuiEntity);
+            self.HidePanel(fuiEntity.PanelId);
+            self.UnLoadPanel(fuiEntity);
         }
         
-        public static void ClosePanel(this FUIComponent self, Entity entity)
+        public static void ClosePanel<P1>(this FUIComponent self, PanelId panelId, P1 p1)
         {
-            FUIEntity fuiEntity = entity.GetParent<FUIEntity>();
-            self.ClosePanel(fuiEntity);
-        }
-                
-        /// 关闭指定的UI窗口，会Unload资源。
-        public static void ClosePanel(this FUIComponent self, FUIEntity fuiEntity)
-        {
-            self.InnerHidePanel(fuiEntity);
+            FUIEntity fuiEntity = self.GetFirstFUIEntityByPanelId(panelId);
+            self.HidePanel(fuiEntity.PanelId, p1);
             self.UnLoadPanel(fuiEntity);
-            Log.Info("<color=magenta>## close panel without Pop ##</color>");
         }
+        
+        public static void ClosePanel<P1, P2>(this FUIComponent self, PanelId panelId, P1 p1, P2 p2)
+        {
+            FUIEntity fuiEntity = self.GetFirstFUIEntityByPanelId(panelId);
+            self.HidePanel(fuiEntity.PanelId, p1, p2);
+            self.UnLoadPanel(fuiEntity);
+        }
+        
+        public static void ClosePanel<P1, P2, P3>(this FUIComponent self, PanelId panelId, P1 p1, P2 p2, P3 p3)
+        {
+            FUIEntity fuiEntity = self.GetFirstFUIEntityByPanelId(panelId);
+            self.HidePanel(fuiEntity.PanelId, p1, p2, p3);
+            self.UnLoadPanel(fuiEntity);
+        }
+        
+        public static void ClosePanel<P1, P2, P3, P4>(this FUIComponent self, PanelId panelId, P1 p1, P2 p2, P3 p3, P4 p4)
+        {
+            FUIEntity fuiEntity = self.GetFirstFUIEntityByPanelId(panelId);
+            self.HidePanel(fuiEntity.PanelId, p1, p2, p3, p4);
+            self.UnLoadPanel(fuiEntity);
+
+        }
+        
+        public static void ClosePanel<P1, P2, P3, P4, P5>(this FUIComponent self, PanelId panelId, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5)
+        {
+            FUIEntity fuiEntity = self.GetFirstFUIEntityByPanelId(panelId);
+            self.HidePanel(fuiEntity.PanelId, p1, p2, p3, p4, p5);
+            self.UnLoadPanel(fuiEntity);
+        }
+
 
         /// 卸载指定的UI窗口实例
         private static void UnLoadPanel(this FUIComponent self, FUIEntity fuiEntity, bool isDispose = true)
